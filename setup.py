6c37c29124bae221032f78252ba6141d17091d72
Parikshit <parik.sonwane06@gmail.com>
2025-08-09 18:22:29 +0530

    update setup

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import setup, find_packages
 
+from setuptools import setup, find_packages
+
 setup(
-    name="dd",
+    name="dd",  # This will be the pip install name
     version="0.1.0",
-    author="Parikshit Sonwane",
+    author="ParikshitSonwane",
     author_email="parik.sonwane06@gmail.com",
     description="A simple CLI Morse Code encoder/decoder",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
-            "dit-dah=dit_dah.cli:main"
+            "dd=dot-dash.main:main"
         ]
     },
     python_requires=">=3.6",
